8bd5a47d7e5d46c2d092f169937eb67ccca1438f
Max <maxawiens@gmail.com>
2021-02-03 15:16:39 -0600

    Cleaned up InputManager

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -8,112 +8,31 @@ using System.Text.Json.Serialization;
 using System.IO;
 
 namespace MainGame.Input {
-	using InputIdentifier = ValueTuple<InputSource, ValueType>;
-	public delegate void InputAction(InputData data);
-
 	public class InputManager {
-		public event Action<float> Moved;
-
-		private Dictionary<string, List<Binding>> _actionBindings;
-
-		private KeyboardState _previousKeyboardState;
-		private Keys[] _previousKeysDown = new Keys[0];
-
-		private GamePadData _previousGamePadData;
-		private MouseState _previousMouseState;
-		private Dictionary<InputIdentifier, InputAction> _actionMap;
-
-		private Dictionary<string, Action> _actions;
+		private Dictionary<string, InputAction> _actions;
 
 		public InputManager() {
-			_actionBindings = new Dictionary<string, List<Binding>>();
-
-			_actionMap = new Dictionary<InputIdentifier, InputAction>() {
-				{(InputSource.Keyboard, Keys.A), DebugPrintKey},
-				{(InputSource.Keyboard, Keys.D), DebugPrintKey},
-			};
-			_actions = new Dictionary<string, Action>();
-
+			_actions = new Dictionary<string, InputAction>();
 			LoadBindings(@"Content\Controlls.json");
 		}
 
 		public void AddListener(string actionName, Action<Vector2> inputAction) {
-			if(_actions.TryGetValue(actionName, out Action action)) {
+			if(_actions.TryGetValue(actionName, out InputAction action)) {
 				action.ValueChanged += inputAction;
 			}
 		}
 
 		public void RemoveListener(string actionName, Action<Vector2> inputAction) {
-			if(_actions.TryGetValue(actionName, out Action action)) {
+			if(_actions.TryGetValue(actionName, out InputAction action)) {
 				action.ValueChanged -= inputAction;
 			}
 		}
 
-		private void DebugPrintKey(InputData data) {
-			System.Diagnostics.Debug.WriteLine(data.State);
-			Moved?.Invoke(data.ReadFloat());
-		}
-
 		public void Update(float deltaTime) {
 			InputState state = new InputState(Keyboard.GetState(), Mouse.GetState(), GamePad.GetState(0));
-			System.Diagnostics.Debug.WriteLine($"{state.GamePad.ThumbSticks.Left},\t {_actions["Move"].PollValue(state)}");
-			foreach(Action action in _actions.Values) {
+			foreach(InputAction action in _actions.Values) {
 				action.Update(state);
 			}
-		}
-
-		private struct GamePadData {
-			public readonly GamePadButtons Buttons;
-			public readonly ButtonState A;
-			public readonly ButtonState B;
-			public readonly ButtonState X;
-			public readonly ButtonState Y;
-			public readonly ButtonState Start;
-			public readonly ButtonState Back;
-			public readonly ButtonState BigButton;
-			public readonly ButtonState LeftShoulder;
-			public readonly ButtonState RightShoulder;
-			public readonly ButtonState LeftStickPress;
-			public readonly ButtonState RightStickPress;
-			public readonly GamePadTriggers Triggers;
-			public readonly float LeftTrigger;
-			public readonly float RightTrigger;
-			public readonly GamePadDPad DPad;
-			public readonly ButtonState Left;
-			public readonly ButtonState Right;
-			public readonly ButtonState Up;
-			public readonly ButtonState Down;
-			public readonly GamePadThumbSticks Sticks;
-			public readonly Vector2 LeftStick;
-			public readonly Vector2 RightStick;
-			public readonly GamePadState State;
-			public GamePadData(GamePadState state) {
-				Buttons = state.Buttons;
-				A = Buttons.A;
-				B = Buttons.B;
-				X = Buttons.X;
-				Y = Buttons.Y;
-				Start = Buttons.Start;
-				Back = Buttons.Back;
-				BigButton = Buttons.BigButton;
-				LeftShoulder = Buttons.LeftShoulder;
-				RightShoulder = Buttons.RightShoulder;
-				LeftStickPress = Buttons.LeftStick;
-				RightStickPress = Buttons.RightStick;
-				Triggers = state.Triggers;
-				LeftTrigger = Triggers.Left;
-				RightTrigger = Triggers.Right;
-				DPad = state.DPad;
-				Left = DPad.Left;
-				Right = DPad.Right;
-				Up = DPad.Up;
-				Down = DPad.Down;
-				Sticks = state.ThumbSticks;
-				LeftStick = Sticks.Left;
-				RightStick = Sticks.Right;
-				State = state;
-			}
-
 		}
 
 		public void LoadBindings(string filePath) {
@@ -139,17 +58,17 @@ namespace MainGame.Input {
 								throw new JsonException();
 						}
 					}
-					_actions.Add(actionJson.Name, new Action(actionJson.Name, bindings));
+					_actions.Add(actionJson.Name, new InputAction(actionJson.Name, bindings));
 				}
 			}
 		}
 
-		private class Action {
+		private class InputAction {
 			public readonly string Name;
 			private Vector2 _previousValue;
 			private IEnumerable<Binding> _bindings;
 			public event Action<Vector2> ValueChanged;
-			public Action(string name, IEnumerable<Binding> bindings) {
+			public InputAction(string name, IEnumerable<Binding> bindings) {
 				Name = name;
 				_bindings = bindings;
 				_previousValue = Vector2.Zero;
---
+++ Util/XnaExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+namespace MainGame.Util {
+	public static class XnaExtensions {
+		public static float AsFloat(this Vector2 value)
+			=> value.X != 0 ? value.X : value.Y;
+		public static bool AsBool(this Vector2 value)
+			=> value != Vector2.Zero;
+	}
+}
